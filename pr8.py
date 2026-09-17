5d4c731116759157e020df83c00ed9e1f0363229
Ansh Golakiya <anshgolakiya9909@gmail.com>
2026-09-18 05:25:04 +0530

    Add age statistics calculations for employees

---
+++ pr8.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df = pd.read_csv("employee.csv")
+
+data = df["Age"].dropna()
+
+#mean
+print("Mean: ", df["Age"].mean())
+
+#describe()
+print(df["Age"].describe())
+
+#median
+print("Median: ", df["Age"].median())
+
+#mode
+print("Mode: ", df["Age"].mode())
+
+#range
+max = df["Age"].max()
+min = df["Age"].min()
+print("age range of employee in company: ", min , "to", max)
+
+#iqr of age column
+q1 = df["Age"].quantile(0.25)
+q3 = df["Age"].quantile(0.75)
+print("IQR of Age: ", q3 - q1)
